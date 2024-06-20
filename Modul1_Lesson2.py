79074c85f09a6b610129263b82b8d7c4b0e2a627
Diana091095 <diana091095@mail.ru>
2024-06-20 13:59:48 +0300

    Add files via upload

---
+++ Modul1_Lesson2.py
@@ -0,0 +1,5 @@
+Number_of_homework_completed = 12
+number_of_hours_spent = 1.5
+Course_name = "Python"
+Time_per_task = number_of_hours_spent // Number_of_homework_completed
+print(Course_name,'всего задач;',Number_of_homework_completed,',затрачено часов;',number_of_hours_spent,', среднее время выполнения',number_of_hours_spent / Number_of_homework_completed,'часа')
